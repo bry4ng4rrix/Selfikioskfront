ab62575a7b8f5b29f9ed6b29346c2d19665043ee
bry4ng4rrix <bryanmfb4@gmail.com>
2025-08-20 10:18:35 +0300

    first comit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ajoutez les extensions utilisées dans votre projet
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
